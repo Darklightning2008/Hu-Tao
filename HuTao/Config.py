5c9af4b23d9c2781fd91238094b92a94e5575d20
Ashui <113001130+ashui501@users.noreply.github.com>
2023-10-08 16:34:26 +0530

    Update Config.py

--- HuTao/Config.py
+++ HuTao/Config.py
@@ -8,9 +8,9 @@ LOGGER = getLogger(__name__)
 
 # Required ENV
 try:
-    BOT_TOKEN = "" # BOT TOKEN
-    API_ID =  123 # API ID
-    API_HASH = "" # API HASH
+    BOT_TOKEN = "6316910949:AAHdCdL8Uo2rxep0wsAUL4dWmtzDo7nRIIQ" # BOT TOKEN
+    API_ID =  583311 # API ID
+    API_HASH = "c67090d2c0a37d207385d04d9640045c" # API HASH
 except Exception as e:
     LOGGER.error(f"Looks Like Something Is Missing!! Please Check Variables\n{e}")
     sys.exit(1)
@@ -22,12 +22,12 @@ COMMAND_HANDLER = ". /".split() # COMMAND HANDLER
 
 SUDO = list({int(x)for x in ("").split()})
 
-SUPPORT_CHAT = "NovaSupports" # SUPPORT GROUP (ID OR USERNAME)
+SUPPORT_CHAT = "uchihasasukebotsupport" # SUPPORT GROUP (ID OR USERNAME)
 
-LOG_CHANNEL_ID = -1001816188874 #LOG GROUP ID FOR YOUR BOT
+LOG_CHANNEL_ID = -1001825488060 #LOG GROUP ID FOR YOUR BOT
 
-OWNER = list({int(x)for x in ("6149191605").split()}) #OWNER ID
+OWNER = list({int(x)for x in ("5829077962").split()}) #OWNER ID
 
-DB_URL = "" # MONGO DB URL
+DB_URL = "mongodb+srv://DarkLightning2008:Darkdeebu2008@cluster1.ut0l8mm.mongodb.net/?retryWrites=true&w=majority" # MONGO DB URL
 
-SQL_URL = "" # ELEPHANT SQL URL
+SQL_URL = "postgres://mlckhdzh:***@tiny.db.elephantsql.com/mlckhdzh" # ELEPHANT SQL URL
